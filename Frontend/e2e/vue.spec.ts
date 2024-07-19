de4696c2bb0e213c69a6b7416dea0ab4e299cfde
JoachimTislov <joachim.tislov@gmail.com>
2024-07-19 23:26:55 +0200

    built the SPA

--- Frontend/e2e/vue.spec.ts
+++ Frontend/e2e/vue.spec.ts
@@ -1,8 +1,21 @@
-import { test, expect } from '@playwright/test';
-
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-})
+
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
