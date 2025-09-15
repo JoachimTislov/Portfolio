bdb50ec32084121fcc46c5b08699fb8aa6bf70cc
JoachimTislov <joachim.tislov@gmail.com>
2025-09-15 06:12:47 +0200

    add theme toggling support - dark/white

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,27 @@ import { RouterView, useRoute } from 'vue-router'
 import routes from '@/router/routes'
 import ArrowLink from './components/Portfolio/ArrowLink.vue'
 import { iconDirection } from './logic/Portfolio/enums'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { ref } from 'vue'
+const darkMode = ref(
+  localStorage.getItem('darkMode') !== null ||
+    (!('darkMode' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+)
+function toggleTheme() {
+  if (darkMode.value) {
+    document.documentElement.classList.remove('dark')
+    localStorage.removeItem('darkMode')
+  } else {
+    localStorage.setItem('darkMode', 'enabled')
+    document.documentElement.classList.add('dark')
+  }
+  darkMode.value = !darkMode.value
+}
 </script>
 
 <template>
-  <div class="flex justify-center mb-10">
+  <div class="mb-10 flex justify-center">
     <div class="flex flex-col">
       <div class="mt-2 flex h-10 justify-center gap-x-5 lg:justify-between">
         <ArrowLink
@@ -19,6 +36,14 @@ import { iconDirection } from './logic/Portfolio/enums'
           arrow-left-side
         />
         <SocialLinks />
+        <span
+          class="dark:text-lightgray dark:hover:text-darkorange text-3xl text-gray-500 transition-colors hover:cursor-pointer hover:text-blue-400"
+        >
+          <font-awesome-icon
+            @click="toggleTheme"
+            :icon="darkMode ? faSun : faMoon"
+          />
+        </span>
       </div>
       <div class="max-w-302 mt-5">
         <RouterView />
