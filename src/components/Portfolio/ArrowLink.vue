6e837663316c68e4f4eeb15770c2e466c2b3e0f5
JoachimTislov <joachim.tislov@gmail.com>
2025-10-05 02:12:27 +0200

    remove touch interactivity in arrowlink comp

--- src/components/Portfolio/ArrowLink.vue
+++ src/components/Portfolio/ArrowLink.vue
@@ -17,19 +17,17 @@ const showArrow = ref(false)
     v-if="href || to"
     @mouseenter="showArrow = true"
     @mouseleave="showArrow = false"
-    @touchstart="showArrow = true"
-    @touchend="showArrow = false"
-    @touchcancel="showArrow = false"
     @focus="showArrow = true"
     @blur="showArrow = false"
     :is="href ? 'a' : 'RouterLink'"
     :to
     :href
-    class="text-md flex items-center p-2 font-mono hover:bg-gray-300/80 active:bg-gray-400/80 dark:hover:bg-zinc-900 dark:active:bg-zinc-800"
+    class="text-md flex items-center rounded-sm p-2 font-mono hover:bg-gray-300/80 active:bg-gray-400/80 dark:hover:bg-zinc-900 dark:active:bg-zinc-800"
     :class="[
       arrowLeftSide ? 'flex-row-reverse' : '',
       to ? 'hover:cursor-pointer' : ''
     ]"
+    v-bind="href ? { target: '_blank', rel: 'noopener' } : {}"
   >
     {{ name }}
     <font-awesome-icon
